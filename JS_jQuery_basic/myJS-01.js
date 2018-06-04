98b36e0534d5c2b41a84e5c86d52acc363545e1e
EC2 Default User <ec2-user@ip-172-31-1-100.us-east-2.compute.internal>
2018-06-04 03:27:36 +0000

    add all

--- JS_jQuery_basic/myJS-01.js
+++ JS_jQuery_basic/myJS-01.js
@@ -3,11 +3,23 @@ this is my first
 JS external file
 */
 
+//It's a good programming practice to declare all variables at the beginning of a script.
+var x, y, z;
+x = "10"; //If you put a number in quotes, the rest of the numbers will be treated as strings, and concatenated.
+y = 20;
+z = x + y;
+
 function myAnotherFunc(){
     document.getElementById("demo").innerHTML = z;
 }
 
-var x, y, z;
-x = 10;
-y = 20;
-z = x + y;
+function myObject(){
+    var myStuff = {
+        "name": "raiden",
+        "sso": "302015572"
+    }
+    var my_undefined;
+    var my_null;
+    my_null = null; // Now value is null, but type is still an object
+    document.getElementById("demo").innerHTML = myStuff.name + ", type is: " + typeof(myStuff.name) + ', myundeined item type is: ' + typeof(my_undefined) + ", myNull item type is: " + typeof(my_null)
+}
